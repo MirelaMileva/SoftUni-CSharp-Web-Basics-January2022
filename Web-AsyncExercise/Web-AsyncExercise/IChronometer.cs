1d8fc9f4faa3d00be8aa0b9db7f7df284785b9ae
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2022-01-19 11:59:25 +0000

    Add New Files

---
+++ Web-AsyncExercise/Web-AsyncExercise/IChronometer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Web_AsyncExercise
+{
+    public interface IChronometer
+    {
+        string GetTime { get; }
+
+        List<string> Laps { get; }
+
+        void Start();
+
+        void Stop();
+
+        string Lap();
+
+        void Reset();
+    }
+}
